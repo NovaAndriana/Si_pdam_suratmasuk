14373442662749dd1e5e13bd171273958dad1b2e
NovaAndriana <novaandriana.kartawijaya@gmail.com>
2018-09-23 17:23:10 +0700

    Initial commit

---
+++ src/si_pdam_suratmasuk/Si_pdam_suratmasuk.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package si_pdam_suratmasuk;
+
+/**
+ *
+ * @author novaa
+ */
+public class Si_pdam_suratmasuk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
